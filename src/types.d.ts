0e87094024a2120433a3e6d1efa78a1cb809e36f
George Nikolopoulos <georgen@interactive-design.gr>
2025-05-21 01:54:15 +0300

    Added SEO meta component

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,3 +10,36 @@ export interface Box {
   content: string;
   link?: Link;
 }
+
+export interface MetaData {
+  title?: string;
+  robots?: MetaDataRobots;
+  description?: string;
+  openGraph?: MetaDataOpenGraph;
+  twitter?: MetaDataTwitter;
+}
+
+export interface MetaDataRobots {
+  index?: boolean;
+  follow?: boolean;
+}
+
+export interface MetaDataImage {
+  url: string;
+  width?: number;
+  height?: number;
+}
+
+export interface MetaDataOpenGraph {
+  url?: string;
+  siteName?: string;
+  images?: Array<MetaDataImage>;
+  locale?: string;
+  type?: string;
+}
+
+export interface MetaDataTwitter {
+  handle?: string;
+  site?: string;
+  cardType?: string;
+}
