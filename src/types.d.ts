410f774bd55fc75b2ef4aab5b194e6503a152a6d
George Nikolopoulos <georgen@interactive-design.gr>
2025-05-21 01:17:25 +0300

    Images in src

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,3 @@
-export interface Image {
-  src: string;
-  alt: string;
-  width?: string;
-  height?: string;
-}
-
 export interface Link {
   href: string;
   target?: string;
@@ -13,7 +6,6 @@ export interface Link {
 
 export interface Box {
   size?: string;
-  image: Image;
   overlayColor: string;
   content: string;
   link?: Link;
